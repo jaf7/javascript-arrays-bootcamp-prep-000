0bdd03cd4a16542c2b69ed803f4f56ec13292101
Anthony Fields <janthonyfields@gmail.com>
2017-09-09 19:06:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray( array, element ) {
   return array;
 }
 
-function addElementToEndOfArray( element, array ) {
+function addElementToEndOfArray( array, element ) {
   return [...array, element ];
 }
 
